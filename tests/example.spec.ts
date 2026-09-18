1f570a03ad914f71e25a91160a33ce35edc62104
Sravani <sravanibhumireddy2407@gmail.com>
2026-09-18 10:34:48 +0530

    Added login scenarios

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -16,3 +16,8 @@ test('get started link', async ({ page }) => {
   // Expects page to have a heading with the name of Installation.
   await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
 });
+
+test('get started link', async ({ }) => {
+ console.log("Code from user2");
+});
+
